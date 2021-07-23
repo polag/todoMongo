031fc758587fede8dce86e05eefa0f7bf79808f7
Paula <paula.goicoechea@gmail.com>
2021-07-23 09:26:52 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -4,6 +4,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 $client = new MongoDB\Client(
    
     //"mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
-   "mongodb+srv://paula:luna12182@cluster0.bsigg.mongodb.net/blog?retryWrites=true&w=majority"
+   "mongodb+srv://paula:password2@cluster0.bsigg.mongodb.net/blog?retryWrites=true&w=majority"
    
 );
